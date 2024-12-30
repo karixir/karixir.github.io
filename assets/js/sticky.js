42ce48fb7fd8fbd8e9d563e2fbb663072d1a6c7b
Karina Zawada <karinazawada@student.agh.edu.pl>
2024-12-30 20:55:43 +0100

    Delete assets/js/sticky.js

--- assets/js/sticky.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function() {
-    var header = document.querySelector('.sticky-header');
-    if (window.scrollY > 100) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-};
